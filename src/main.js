828e0c8b3ddb34381e69ba0935169653f0aecb99
Mark Wylde <mark.wylde@legalease.co.uk>
2024-09-16 09:28:36 +0100

    feat: use env port

--- src/main.js
+++ src/main.js
@@ -127,7 +127,7 @@ const server = http.createServer(async (req, res) => {
   }
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
